1475bef137a136204eab3769e590a135e0e461f8
99003613 <78539802+99003613@users.noreply.github.com>
2021-03-06 13:33:57 +0530

    Delete mystring.h

--- include/mystring.h
+++
@@ -1,7 +0,0 @@
-#ifndef __MYSTRING_H
-#define __MYSTRING_H
-    int strlen(char* str1);
-    char* mystrcpy(char* str1);
-    char* mystrcat(char* str1, char* str2);
-    int mystrcmp(char* str1, char* str2);
-#endif
