e433cd523bbe99b6eb2b6f286b8d87ba4a4f2289
99003613 <78539802+99003613@users.noreply.github.com>
2021-03-06 13:50:47 +0530

    Delete mystrcat.c

--- src/stringfunctions/mystrcat.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<string.h>
-
-void string_cat(char str[])
-{
-    char str2[40]= "Hello" ;
-    strcat(str2,str);
-    printf("String 1 concatenated with string 2, String2 = %f\n",str2);
-}
